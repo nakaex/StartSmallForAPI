14ebde0e2d7a9fb7ccb2d4f4d2e1a7c56fbb4a9d
中江洋史 <h_nakae@nus21.com>
2020-09-09 21:30:01 +0900

    first commit

---
+++ StartSmallForAPI/WebAPI.swift
@@ -0,0 +1,9 @@
+//
+//  WebAPI.swift
+//  StartSmallForAPI
+//
+//  Created by 中江洋史 on 2020/09/02.
+//  Copyright © 2020 中江洋史. All rights reserved.
+//
+
+import Foundation
---
+++ StartSmallForAPITests/GitHubAPITests.swift
@@ -0,0 +1,9 @@
+//
+//  GitHubAPITests.swift
+//  StartSmallForAPITests
+//
+//  Created by 中江洋史 on 2020/09/02.
+//  Copyright © 2020 中江洋史. All rights reserved.
+//
+
+import Foundation
